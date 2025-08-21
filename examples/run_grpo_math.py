cff451fa982de22862dfeb742174eaaf1084a4cc
Dheeraj Peri <peri.dheeraj@gmail.com>
2025-08-21 15:35:32 -0700

    chore: fix prompt_file issue and disable leave_one_out baseline
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- examples/run_grpo_math.py
+++ examples/run_grpo_math.py
@@ -82,7 +82,9 @@ def hf_data_processor(
     message_log: LLMMessageLogType = []
     user_message = {
         "role": "user",
-        "content": task_data_spec.prompt.format(problem),
+        "content": task_data_spec.prompt.format(problem)
+        if task_data_spec.prompt
+        else problem,
     }
     message: list[str] = tokenizer.apply_chat_template(  # type: ignore
         [user_message],
